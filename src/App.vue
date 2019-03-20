39e65d29585e39131e6a1be916c8266625e70387
Nicolai Berthelsen <Nicolai.Berthelsen@komplett.com>
2019-03-20 21:00:31 +0100

    starting out

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
@@ -25,4 +24,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+  background-color: #fff;
+}
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <form v-on:submit.prevent="onSubmit">
+      <label>
+        <span>Mobilnummer</span>
+        <input type="text" v-model="number" />
+      </label>
+      <label>
+        <span>Passord</span>
+        <input type="password" v-model="password" />
+      </label>
+      <button type="submit">Logg inn</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'Login',
+  data: function() {
+    return {
+      number: "",
+      password: ""
+    }
+  },
+  methods: {
+    onSubmit() {
+      axios.post("https://mykid.no/forside/forside/my_brukere_logginn", {
+        mykid_username: "40469957",
+        mykid_password: "hkbsb4"
+      }).then(response => console.log(response))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .container {
+
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    label {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      padding-bottom: 12px;
+
+      span {
+        font-weight: bold;
+        padding-bottom: 4px;
+      }
+    }
+
+    button {
+        background-color: #2196f3;
+        color: #fff;
+        padding: 6px 16px;
+        min-width: 64px;
+        transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+        line-height: 1.75;
+        font-weight: 500;
+        border-radius: 4px;
+        text-transform: uppercase;
+        letter-spacing: 0.02857em;
+        box-shadow: 0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12);
+        border: 0;
+        cursor: pointer;
+
+        &:hover {
+          background-color: #1976d2;
+        }
+      }
+  }
+</style>
