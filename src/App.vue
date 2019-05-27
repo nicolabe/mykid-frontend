6a849479828f9b834eb019c06511b26164d897b9
Nicolai Berthelsen <Nicolai.Berthelsen@komplett.com>
2019-05-27 11:33:53 +0200

    some better error handling

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,32 @@
 <template>
   <div id="app">
-    <Navbar
-      v-bind:user="user"
-      v-on:logout="logout"
-    />
-    <div class="alert alert-danger" v-if="errorMsg" role="alert">
-      {{errorMsg}}
-    </div>
-    <Login
-      v-if="!user" v-on:setUser="setUser"
-      v-on:error="onError"
-    />
+    <Navbar v-bind:user="user" v-on:logout="logout"/>
+    <div class="alert alert-danger" v-if="errorMsg" role="alert">{{errorMsg}}</div>
+    <Login v-if="!user" v-on:setUser="setUser" v-on:error="onError"/>
     <div v-if="children.length > 0" class="child-container">
       <div class="child-wrapper" v-for="child in children" v-bind:key="child.id">
-        <Child v-bind:child="child" />
+        <Child v-bind:child="child"/>
       </div>
     </div>
 
     <div v-if="plannings.length > 0">
-      <PlanningDays v-bind:days="plannings" />
+      <PlanningDays v-bind:days="plannings"/>
     </div>
   </div>
 </template>
 
 <script>
-import Login from './components/Login.vue'
-import Navbar from './components/Navbar.vue'
-import Child from './components/Child.vue'
-import PlanningDays from "./components/PlanningDays.vue"
+import Login from "./components/Login.vue";
+import Navbar from "./components/Navbar.vue";
+import Child from "./components/Child.vue";
+import PlanningDays from "./components/PlanningDays.vue";
 
-import axios from "axios"
+import axios from "axios";
 
 const MYKID_COOKIE = "mykid";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Login,
     Child,
@@ -47,20 +39,20 @@ export default {
       children: [],
       plannings: [],
       errorMsg: null
-    }
+    };
   },
   methods: {
     setUser(data) {
       this.user = data;
       const jsonData = JSON.stringify(Object.assign({}, data));
-      $cookies.set(MYKID_COOKIE, jsonData, 60*60*24*30, "/");
+      $cookies.set(MYKID_COOKIE, jsonData, 60 * 60 * 24 * 30, "/");
     },
     logout() {
       this.user = null;
       this.children = [];
       this.plannings = [];
       this.errorMsg = null;
-      $cookies.remove(MYKID_COOKIE)
+      $cookies.remove(MYKID_COOKIE);
     },
     onError(errorMsg) {
       this.errorMsg = errorMsg;
@@ -75,29 +67,34 @@ export default {
   watch: {
     user: function(newVal, oldVal) {
       if (newVal && oldVal === null) {
-        axios.get("/api/children")
-          .then(res => this.children = res.data)
-          .catch(err => this.errorMsg = err)
+        axios
+          .get("/api/children")
+          .then(res => (this.children = res.data))
+          .catch(err => {
+            this.errorMsg = "Noe gikk galt, forsøk og logg inn på nytt";
+            this.logout();
+          });
       }
     },
     children: function(newVal, oldVal) {
       if (newVal.length > 0 && oldVal.length === 0) {
-        const childId = newVal[0].id
-        const date = new Date()
-        axios.get("/api/plannings", {
-          params: {
-            child_id: childId,
-            date: date.toISOString().substring(0, 10)
-          }
-        })
-          .then(res => {
-            this.plannings = res.data.planning_days
+        const childId = newVal[0].id;
+        const date = new Date();
+        axios
+          .get("/api/plannings", {
+            params: {
+              child_id: childId,
+              date: date.toISOString().substring(0, 10)
+            }
           })
-          .catch(err => this.errorMsg = err)
+          .then(res => {
+            this.plannings = res.data.planning_days;
+          })
+          .catch(err => (this.errorMsg = err));
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
@@ -118,7 +115,6 @@ export default {
 
 .child-container {
   display: flex;
-
 }
 
 .child-wrapper {
@@ -129,5 +125,4 @@ body {
   background-color: #fff;
   font-family: Roboto;
 }
-
 </style>
